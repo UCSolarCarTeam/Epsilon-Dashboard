015251365303dc413b94d0a830ce41a73fe031cc
Jeffery <jeffhuo77@gmail.com>
2018-01-26 14:13:24 -0700

    Fix retrieving messages

--- src/CommunicationLayer/CommDeviceControl/InternetCommDevice.cpp
+++ src/CommunicationLayer/CommDeviceControl/InternetCommDevice.cpp
@@ -13,9 +13,10 @@ void InternetCommDevice::setChannel(AmqpClient::Channel::ptr_t channel)
 
 void InternetCommDevice::run()
 {
+    std::string consumer_tag = channel_->BasicConsume(queueName_.toStdString(), "", true, true, false);
+
     while (1)
     {
-        std::string consumer_tag = channel_->BasicConsume(queueName_.toStdString(), "", true, true, false);
         AmqpClient::Envelope::ptr_t envelope = channel_->BasicConsumeMessage(consumer_tag);
         QString str = QString::fromStdString(envelope->Message()->Body());
         QByteArray dat = str.toUtf8();
