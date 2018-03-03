ff3ede66c1fcc087edb9b5ceb497703be8ba05be
henrytran28 <henrytrann28@gmail.com>
2018-03-03 16:13:26 -0700

    finished design portion of the UI

--- src/ViewLayer/DebugDisplay/HomePage/HomePageUi/HomePageUi.cpp
+++ src/ViewLayer/DebugDisplay/HomePage/HomePageUi/HomePageUi.cpp
@@ -1,11 +1,12 @@
 #include "ui_HomePageUi.h"
 #include "HomePageUi.h"
+
 HomePageUi::HomePageUi() :
     ui_(new Ui::HomePageUi)
 {
     ui_->setupUi(this);
     // https://stackoverflow.com/questions/19939938
-    QPixmap background(":/Resources/Background.png");
+    QPixmap background(":/Resource/Background.png");
     background = background.scaled(this->size(), Qt::IgnoreAspectRatio);
     QPalette palette;
     palette.setBrush(QPalette::Background, background);
