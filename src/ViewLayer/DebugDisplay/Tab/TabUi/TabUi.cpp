6c8b55b1630d0f8f80a413b31c098ea3b44df3d8
Tehseen Iqbal <tehseen.iqbal@ucalgary.ca>
2018-04-27 22:08:23 -0600

    SolarCarIcon

--- src/ViewLayer/DebugDisplay/Tab/TabUi/TabUi.cpp
+++ src/ViewLayer/DebugDisplay/Tab/TabUi/TabUi.cpp
@@ -2,6 +2,7 @@
 #include "ui_TabUi.h"
 #include <QTimer>
 #include <QDateTime>
+#include <QPixmap>
 
 TabUi::TabUi(QWidget *parent) :
     ui_(new Ui::TabUi)
@@ -14,6 +15,24 @@ TabUi::TabUi(QWidget *parent) :
     bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
     QPalette background = palette();
     background.setBrush(QPalette::Background, bkgnd);
+
+    ui_->controlButton->setIcon(QIcon(":/Resources/SteeringIcon.png"));
+    ui_->controlButton->setIconSize(QSize(30,30));
+    ui_->batteryButton->setIcon(QIcon(":/Resources/BatteryIcon.png"));
+    ui_->batteryButton->setIconSize(QSize(30,30));
+    ui_->motorButton->setIcon(QIcon(":/Resources/EngineIcon.png"));
+    ui_->motorButton->setIconSize(QSize(35,35));
+    ui_->faultButton->setIcon(QIcon(":/Resources/GearIcon.png"));
+    ui_->faultButton->setIconSize(QSize(35,35));
+    ui_->mpptButton->setIcon(QIcon(":/Resources/MPPTIcon.png"));
+    ui_->mpptButton->setIconSize(QSize(38,38));
+
+    QPixmap pix(":/Resources/MPPTIcon.png");
+    //correct logo below
+    //QPixmap pix(":/Resources/SolarCarTeam.png");
+    //
+    pix = pix.scaled(QSize(25,25));
+    ui_->calgaryTeam->setPixmap(pix);
 
     this->setAutoFillBackground(true);
     this->setPalette(background);
