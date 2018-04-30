1c3b09445ea249f2458171aafb0cb9f758e44eaf
Ben-Graham <bengraham373@gmail.com>
2018-04-30 16:32:22 -0600

    Control Debug Display

--- src/ViewLayer/ViewContainer.cpp
+++ src/ViewLayer/ViewContainer.cpp
@@ -69,7 +69,7 @@ ViewContainer::ViewContainer(PresenterContainer& presenterContainer, Mode mode)
 
 
         MpptView_.reset(new MpptView(presenterContainer.mpptPresenter(), *mpptUi_));
-        ControlView_.reset(new ControlView(presenterContainer.driverControlsPresenter(), *controlUi_));
+        ControlView_.reset(new ControlView(presenterContainer.driverControlsPresenter(), presenterContainer.lightsPresenter(), *controlUi_));
         HomePageView_.reset(new HomePageView(*homepageUi_));
     }
 }
