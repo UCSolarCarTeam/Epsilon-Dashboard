211867a0f4f7df68384f0365afa98006053dcd2b
mglyngberg <mglyngberg2@gmail.com>
2018-05-14 19:44:21 -0600

    formatting

--- src/ViewLayer/Faults/FaultPriorities/FaultPriorities.h
+++ src/ViewLayer/Faults/FaultPriorities/FaultPriorities.h
@@ -7,7 +7,8 @@ namespace FaultPriorities
     enum PRIORITY {INVALID_PRIORITY = -1,
                    HIGH_PRIORITY = 0,
                    MEDIUM_PRIORITY = 1,
-                   LOW_PRIORITY = 2};
+                   LOW_PRIORITY = 2
+                  };
 
     const QColor HIGH_PRIORITY_COLOR = QColor("#ff0000");     //Red
     const QColor MEDIUM_PRIORITY_COLOR = QColor("#ccbf14");   //Yellow
