584d1311f0fd26210e3d5e8e36a4d1e932baa599
Baba Adeniran <babaadeniran@Babas-MacBook-Pro.local>
2018-06-23 18:18:48 -0400

    Using QLock to prevent multiple instances of dashboard

--- src/EpsilonDashboard/main.cpp
+++ src/EpsilonDashboard/main.cpp
@@ -1,8 +1,28 @@
 #include "EpsilonDashboard.h"
 #include <QApplication>
+#include <QLockFile>
+#include <QDebug>
+#include <QDir>
+#include <QMessageBox>
 
 int main(int argc, char* argv[])
 {
+    QLockFile lockFile(QDir::temp().absoluteFilePath("epsilonDashboard.lock"));
+
+    /* Trying to close the Lock File, if the attempt is unsuccessful for 100 milliseconds,
+     * then there is a Lock File already created by another process.
+     * Therefore, we throw a warning and close the program
+     */
+    lockFile.setStaleLockTime(5000);
+    if(!lockFile.tryLock(100)){
+        QMessageBox msgBox;
+        msgBox.setIcon(QMessageBox::Warning);
+        msgBox.setText("The application is already running.\n"
+                       "Allowed to run only one instance of the application.");
+        msgBox.exec();
+        return 1;
+    }
+
     QScopedPointer<EpsilonDashboard> app;
     app.reset(new EpsilonDashboard(argc, argv));
     return app->exec();
