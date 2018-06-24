8d3817d537790b4fd085b08f8d72108b79024bf3
badeniran <baba.adeniran@gmail.com>
2018-06-23 18:41:45 -0600

    Working

--- src/EpsilonDashboard/main.cpp
+++ src/EpsilonDashboard/main.cpp
@@ -1,7 +1,8 @@
 #include "EpsilonDashboard.h"
 #include <QApplication>
-#include <QMessageBox>
+#include <QDebug>
 #include <QSystemSemaphore>
+#include <QSharedMemory>
 
 int main(int argc, char* argv[])
 {
@@ -11,27 +12,35 @@ int main(int argc, char* argv[])
     // on linux/unix shared memory is not freed upon crash
     // so if there is any trash from previous instance, clean it
     QSharedMemory nix_fix_shmem("epsilonDashboardSharedMem");
-    if(nix_fix_shmem.attach()){
+
+    if (nix_fix_shmem.attach())
+    {
         nix_fix_shmem.detach();
     }
 
     QSharedMemory shmem("epsilonDashboardSharedMem");
     bool is_running;
-    if (shmem.attach()){
+
+    if (shmem.attach())
+    {
         is_running = true;
-    }else{
+    }
+    else
+    {
         shmem.create(1);
         is_running = false;
     }
+
     sema.release();
 
-    if(is_running){
-        QMessageBox msgBox;
-        msgBox.setIcon(QMessageBox::Warning);
-        msgBox.setText("You already have this app running."
-                        "\r\nOnly one instance is allowed.");
-        msgBox.exec();
+    if (is_running)
+    {
+        qDebug() << "An instance of dashboard already exists.\nQuitting...\n";
         return 1;
+    }
+    else
+    {
+        qDebug() << "No other instance of dashboard exists.\nLaunching dashboard...\n";
     }
 
     QScopedPointer<EpsilonDashboard> app;
