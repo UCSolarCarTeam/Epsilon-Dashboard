2452a040e5261449b15c7445daee76cbca056977
Zelin <kobezhou1998@gmail.com>
2019-05-25 15:30:15 -0600

    Motor current issue

--- src/DataLayer/KeyMotorData/KeyMotorData.cpp
+++ src/DataLayer/KeyMotorData/KeyMotorData.cpp
@@ -20,9 +20,20 @@ KeyMotor KeyMotorData::motorOne() const
 void KeyMotorData::setMotorZero(KeyMotor motorZero)
 {
     motorZero_ = motorZero;
+
     emit motorZeroReceived(motorZero_);
     emit motorSetPowerReceived(((motorZero_.busVoltage() + motorOne_.busVoltage()) / 2) * ((motorZero_.busCurrent() + motorOne_.busCurrent()) / 2));
     emit motorSetCurrentReceived((motorZero_.setCurrent() + motorOne_.setCurrent()) / 2);
+    if(motorZero_.setCurrent()>motorOne_.setCurrent())
+    {
+          emit motorSetCurrentReceived(motorZero_.setCurrent());
+    }
+    else
+    {
+        emit motorSetCurrentReceived(motorOne_.setCurrent());
+    }
+
+
     emit motorActualSpeedReceived((motorZero_.vehicleVelocity() + motorOne_.vehicleVelocity()) / 2);
     emit motorBusVoltageReceived((motorZero_.busVoltage() + motorOne_.busVoltage()) / 2);
     emit motorBusCurrentReceived((motorZero_.busCurrent() + motorOne_.busCurrent()) / 2);
@@ -32,7 +43,16 @@ void KeyMotorData::setMotorOne(KeyMotor motorOne)
     motorOne_ = motorOne;
     emit motorOneReceived(motorOne_);
     emit motorSetPowerReceived(((motorZero_.busVoltage() + motorOne_.busVoltage()) / 2) * ((motorZero_.busCurrent() + motorOne_.busCurrent()) / 2));
-    emit motorSetCurrentReceived((motorZero_.setCurrent() + motorOne_.setCurrent()) / 2);
+
+    if(motorZero_.setCurrent()>motorOne_.setCurrent())
+    {
+          emit motorSetCurrentReceived(motorZero_.setCurrent());
+    }
+    else
+    {
+        emit motorSetCurrentReceived(motorOne_.setCurrent());
+    }
+
     emit motorActualSpeedReceived((motorZero_.vehicleVelocity() + motorOne_.vehicleVelocity()) / 2);
     emit motorBusVoltageReceived((motorZero_.busVoltage() + motorOne_.busVoltage()) / 2);
     emit motorBusCurrentReceived((motorZero_.busCurrent() + motorOne_.busCurrent()) / 2);
