ad741240d9dac2521ead43d1cf9a7c54eda41c71
Bill Luu <billluu619@gmail.com>
2020-05-21 19:24:35 -0600

    SQUASH: travis

--- EpsilonDashboardSetup.sh
+++ EpsilonDashboardSetup.sh
@@ -6,5 +6,5 @@ fi
 
 echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list && sudo apt-get update && sudo apt-get install rabbitmq-server
 apt-get install cmake openssl libssl-dev libblkid-dev e2fslibs-dev libaudit-dev software-properties-common build-essential mesa-common-dev libgl1-mesa-dev python3-pip
-pip3 install conan
+pip3 install --user conan
 conan remote add conan-dbely https://api.bintray.com/conan/dbely/conan
